c942aa8bf14f659cf313da452e0f46f39618d999
Odilbek <gulnozkutubxonasi@gamil.com>
2025-08-01 07:15:29 +0500

    c

--- filters.py
+++ filters.py
@@ -1,6 +1,7 @@
 from aiogram.types import Message
 from aiogram.filters import Filter
 from config import ADMINS
+from models import User
 
 class IsAdmin(Filter):
     async def __call__(self, message: Message) -> bool:
@@ -9,3 +10,7 @@ class IsAdmin(Filter):
 class IsNotAdmin(Filter):
     async def __call__(self, message: Message) -> bool:
         return message.from_user.id not in ADMINS
+    
+class IsNewUser(Filter):
+    async def __call__(self, message: Message):
+        return not await User.get_or_none(message.from_user.id)
--- handlers/user/start.py
+++ handlers/user/start.py
@@ -3,14 +3,20 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from keyboards.user import main_menu
-from filters import IsNotAdmin
+from filters import IsNotAdmin, IsNewUser
+from models import User
 
 router = Router()
+
+@router.message(IsNewUser())
+async def f(message: Message):
+    await User.get_or_create(message.from_user.id, defaults={"name": message.from_user.full_name, "status": "simple"})
+    await message.answer("Assalomu alaykum, xush kelibsiz!", reply_markup=main_menu)
 
 @router.message(Command("start"), IsNotAdmin())
 async def start_command(message: Message, state: FSMContext):
     await state.clear()
-    await message.answer("Assalomu alaykum, xush kelibsiz!", reply_markup=main_menu)
+    await message.answer("Asosiy menyudasiz!", reply_markup=main_menu)
 
 @router.message(F.text == "Bekor qilish", IsNotAdmin())
 async def cancel_command(message: Message, state: FSMContext):
